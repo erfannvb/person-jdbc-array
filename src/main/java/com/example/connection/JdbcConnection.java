e5250b5f95d133dc8312129ea09368f292331d16
Erfan <erfannavab@gmail.com>
2023-06-02 00:34:11 +0330

    - added HikariCP configuration - deleted sql connection

--- src/main/java/com/example/connection/JdbcConnection.java
+++ src/main/java/com/example/connection/JdbcConnection.java
@@ -1,16 +1,31 @@
 package com.example.connection;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.*;
 
 import static com.example.connection.JdbcConstant.*;
 
 public class JdbcConnection {
 
-    private static Connection connection;
+    private static HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(URL);
+        config.setUsername(USERNAME);
+        config.setPassword(PASSWORD);
+        config.setMaximumPoolSize(10);
+        config.setAutoCommit(true);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        dataSource = new HikariDataSource(config);
+    }
 
     public static Connection getConnection() throws SQLException {
-        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        return connection;
+        return dataSource.getConnection();
     }
 
     public static void closeConnection(Connection connection) throws SQLException {
